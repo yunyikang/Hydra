05d4de465273b40c18a470315e4a3f37b8e3dbad
BowSplinter <vivekkalyansk@gmail.com>
2016-11-27 15:49:08 +0800

    Add plant details page using random data

--- app/src/main/java/com/example/yunyi/projecthydra/DetailsActivity.java
+++ app/src/main/java/com/example/yunyi/projecthydra/DetailsActivity.java
@@ -1,8 +1,149 @@
 package com.example.yunyi.projecthydra;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.listener.LineChartOnValueSelectListener;
+import lecho.lib.hellocharts.listener.ViewportChangeListener;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.LineChartView;
+import lecho.lib.hellocharts.view.PreviewLineChartView;
+
 /**
  * Created by Yun YI on 23/11/2016.
  */
 
-public class DetailsActivity {
+public class DetailsActivity extends AppCompatActivity {
+
+    LineChartView chart;
+    PreviewLineChartView previewChart;
+    LineChartData data;
+    LineChartData previewData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        chart = (LineChartView)findViewById(R.id.chart);
+        previewChart = (PreviewLineChartView) findViewById(R.id.preview_chart);
+
+        generateDefaultData();
+
+        chart.setLineChartData(data);
+        chart.setZoomEnabled(false);
+        chart.setScrollEnabled(false);
+        chart.setOnValueTouchListener(new ValueTouchListener());
+
+        previewChart.setLineChartData(previewData);
+        previewChart.setViewportChangeListener(new ViewportListener());
+
+        previewX(true);
+    }
+
+    private void generateDefaultData() {
+        int numValues = 50;
+
+        List<PointValue> values = new ArrayList<PointValue>();
+        for (int i = 0; i < numValues; ++i) {
+            values.add(new PointValue(i, (int) (Math.random() * 100)));
+        }
+
+        Line line = new Line(values);
+        line.setColor(ChartUtils.COLOR_BLUE);
+
+        List<Line> lines = new ArrayList<Line>();
+        lines.add(line);
+
+        values = new ArrayList<PointValue>();
+        for (int i = 0; i < numValues; ++i) {
+            values.add(new PointValue(i, (int) (Math.random() * 100)));
+        }
+
+        line = new Line(values);
+        line.setColor(ChartUtils.COLOR_VIOLET);
+        lines.add(line);
+
+        data = new LineChartData(lines);
+        previewData = new LineChartData(data);
+
+        data.setAxisXBottom(new Axis().setName("Time").setTypeface(Typeface.DEFAULT_BOLD).setTextColor(Color.rgb(120,120,120)));
+        data.setAxisYLeft(new Axis().setHasLines(true).setTypeface(Typeface.DEFAULT_BOLD).setTextColor(Color.rgb(120,120,120)));
+
+        previewData.getLines().get(0).setColor(ChartUtils.DEFAULT_COLOR);
+        previewData.getLines().get(1).setColor(ChartUtils.DEFAULT_COLOR);
+        previewData.getLines().get(0).setHasPoints(false);
+        previewData.getLines().get(1).setHasPoints(false);
+
+
+
+    }
+
+    private class ViewportListener implements ViewportChangeListener {
+
+        @Override
+        public void onViewportChanged(Viewport newViewport) {
+            // don't use animation, it is unnecessary when using preview chart.
+            chart.setCurrentViewport(newViewport);
+        }
+
+    }
+
+    private void previewY() {
+        Viewport tempViewport = new Viewport(chart.getMaximumViewport());
+        float dy = tempViewport.height() / 4;
+        tempViewport.inset(0, dy);
+        previewChart.setCurrentViewportWithAnimation(tempViewport);
+        previewChart.setZoomType(ZoomType.VERTICAL);
+    }
+
+    private void previewX(boolean animate) {
+        Viewport tempViewport = new Viewport(chart.getMaximumViewport());
+        float dx = tempViewport.width() / 3;
+        tempViewport.inset(dx, 0);
+        if (animate) {
+            previewChart.setCurrentViewportWithAnimation(tempViewport);
+        } else {
+            previewChart.setCurrentViewport(tempViewport);
+        }
+        previewChart.setZoomType(ZoomType.HORIZONTAL);
+    }
+
+    private void previewXY() {
+        // Better to not modify viewport of any chart directly so create a copy.
+        Viewport tempViewport = new Viewport(chart.getMaximumViewport());
+        // Make temp viewport smaller.
+        float dx = tempViewport.width() / 4;
+        float dy = tempViewport.height() / 4;
+        tempViewport.inset(dx, dy);
+        previewChart.setCurrentViewportWithAnimation(tempViewport);
+    }
+
+    private class ValueTouchListener implements LineChartOnValueSelectListener {
+
+        @Override
+        public void onValueSelected(int lineIndex, int pointIndex, PointValue value) {
+            Toast.makeText(getApplicationContext(), "Selected: " + value, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onValueDeselected() {
+            // TODO Auto-generated method stub
+
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/example/yunyi/projecthydra/MainActivity.java
+++ app/src/main/java/com/example/yunyi/projecthydra/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.yunyi.projecthydra;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeDetails(View v) {
+        Intent intent = new Intent(this, DetailsActivity.class);
+        startActivity(intent);
     }
 }
