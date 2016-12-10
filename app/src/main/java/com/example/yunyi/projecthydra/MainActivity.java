bbab6c60dc4a9fd46473972b3433d6dbfc8d8f40
BowSplinter <vivekkalyansk@gmail.com>
2016-12-10 22:07:09 +0800

    Take data for graph from firebase

--- app/src/main/java/com/example/yunyi/projecthydra/MainActivity.java
+++ app/src/main/java/com/example/yunyi/projecthydra/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.yunyi.projecthydra;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -109,10 +110,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-//    public void changeDetails(View v) {
-//        Intent intent = new Intent(this, DetailsActivity.class);
-//        startActivity(intent);
-//    }
+    public void changeDetails(View v) {
+        Intent intent = new Intent(this, DetailsActivity.class);
+        startActivity(intent);
+    }
 
 }
 
